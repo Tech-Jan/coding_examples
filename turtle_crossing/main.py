c8f295c588bbf4ea768c146d9b687b6567f4c44c
tech-jan <trewej@hotmail.com>
2022-12-10 10:51:41 +0100

    Turtle Crossing beginning

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -1,14 +1,24 @@
+import random
 import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+tim = Player()
+screen.listen()
+screen.onkey(key="Up",fun=tim.moveup)
+car = CarManager()
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    tim.finish()
+    car.create_car()
+    car.move_cars()
