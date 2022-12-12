3cdf2c3c7c29ef595c6f14bf546a8dcf9ac7294d
tech-jan <trewej@hotmail.com>
2022-12-12 18:07:28 +0100

    comprehensive lists / dictionionarrys examples. nato phonetic alphabet translator

--- comprehensive list/main.py
+++ comprehensive list/main.py
@@ -1,19 +1,31 @@
-import random
+import pandas
 
-numbers = [1,2,3,4,5]
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# row.letter = row["letter"] and row["code"] would be the same as row.code
+my_nato_dict = {row.letter: row["code"] for index, row in data.iterrows()}
+# print(my_nato_dict)
+print([my_nato_dict[letter.upper()] for letter in input("whats ur names") if letter != " "])
 
-numbers2 = [n*n for n in numbers]
-numbers2 = [n**2 for n in numbers]
-numbers3 = [n**2 for n in numbers]
+#
+##
+###
+#### more examples
+###
+##
+#
+numbers = [1, 2, 3, 4, 5]
+
+numbers2 = [n * n for n in numbers]
+numbers2 = [n ** 2 for n in numbers]
+numbers3 = [n ** 2 for n in numbers]
 name = "mybiglongnname"
 name_list = [1 for letta in name]
 name_list = [letta for letta in name]
-my_list = [n*2 for n in range(1,5)]
-crazy_list = [n*3 for n in range (1,5) if n!=3]
-names = ["Alex","Meth","Caroline","Dave","Alex","Fredd",]
-names = ["Alex","Meth","Caroline","Dave","Alex","Freddssa",]
-newlsit = [myname.upper() for myname in names if len(myname)>5]
-
+my_list = [n * 2 for n in range(1, 5)]
+crazy_list = [n * 3 for n in range(1, 5) if n != 3]
+names = ["Alex", "Meth", "Caroline", "Dave", "Alex", "Fredd", ]
+names = ["Alex", "Meth", "Caroline", "Dave", "Alex", "Freddssa", ]
+newlsit = [myname.upper() for myname in names if len(myname) > 5]
 
 weather_c = {
     "Monday": 12,
@@ -25,9 +37,6 @@ weather_c = {
     "Sunday": 24,
 }
 
-weather_f = {key:(temp_c * 9/5) + 32 for (key,temp_c) in weather_c.items()}
+weather_f = {key: (temp_c * 9 / 5) + 32 for (key, temp_c) in weather_c.items()}
 
 print(weather_f)
-
-
-
