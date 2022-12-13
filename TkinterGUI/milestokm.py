7c77fbfb5610ae8798e6eaa28b1d23b3c96a9577
tech-jan <trewej@hotmail.com>
2022-12-13 11:58:27 +0100

    miles to km converter

--- TkinterGUI/milestokm.py
+++ TkinterGUI/milestokm.py
@@ -1,30 +1,32 @@
 from tkinter import *
 
 mywindow = Tk()
-mywindow.minsize(width=220,height=20)
+mywindow.minsize(width=220, height=20)
 mywindow.title("Mile to Km Converter")
-mywindow.config(padx=4,pady=4)
+mywindow.config(padx=4, pady=4)
 
 label1 = Label(text="is equal to")
-label1.grid(column=1,row=1)
+label1.grid(column=1, row=1)
 
 label2 = Label(text="Km")
-label2.grid(column=3,row=1)
+label2.grid(column=3, row=1)
 
 label3 = Label(text="0")
-label3.grid(column=2,row=1)
+label3.grid(column=2, row=1)
 
 myinput = Entry()
-myinput.grid(column=2,row=0)
+myinput.grid(column=2, row=0)
 myinput.config(width=10)
 
+
 def mybutton_click():
-    givenmiles= int(myinput.get())
-    calculated_km = round(givenmiles*1.60934,2)
-    label3.config(text=calculated_km)
+    givenmiles = float(myinput.get())
+    calculated_km = round(givenmiles * 1.60934, 2)
+    label3.config(text=f"{calculated_km}")
+
 
 mybutton = Button(text="Calculate", command=mybutton_click)
 
-mybutton.grid(column=2,row=3)
+mybutton.grid(column=2, row=3)
 
 mywindow.mainloop()
